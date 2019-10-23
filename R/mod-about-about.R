2b02ec2fa195383383cfc02fc9be5b047fab71f1
Elie Arnaud <earnaud@outlook.com>
2019-10-23 14:03:49 +0200

    R files content updated to R-packaging best practices #3

---
+++ R/mod-about-about.R
@@ -0,0 +1,22 @@
+#' @import RefManageR
+about <- function(input, output, session, IM, bibliography){
+  ns <- session$ns
+  
+  library("RefManageR")
+  # source("R/modules/about/about_functions.R")
+  
+  # Bibliography - approximatively the same as 2019 master memoir
+  # bibliography <- list()
+  # bibliography$actors <- ReadBib("R/mod-about-actors.bib")
+  # message("* Actors bibliography successfully loaded !")
+  # bibliography$informatics <- ReadBib("R/mod-about-informatics.bib")
+  # message("* Informatics bibliography successfully loaded !")
+  # bibliography$ecology <- ReadBib("R/mod-about-ecology.bib")
+  # message("* Ecology bibliography successfully loaded !")
+  
+  # use function
+  output$actors <- renderBibliography(bibliography$actors)
+  output$informatics <- renderBibliography(bibliography$informatics)
+  output$ecology <- renderBibliography(bibliography$ecology)
+  
+}
