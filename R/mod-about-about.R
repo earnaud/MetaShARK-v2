33b2ec5f93dd281977e4673232d88bbee9d68d9b
Elie Arnaud <earnaud@outlook.com>
2019-10-23 13:51:40 +0200

    Updated data with 'about' bibliographies

--- R/mod-about-about.R
+++ R/mod-about-about.R
@@ -6,13 +6,13 @@ about <- function(input, output, session, IM, bibliography){
   # source("R/modules/about/about_functions.R")
   
   # Bibliography - approximatively the same as 2019 master memoir
-  bibliography <- list()
-  bibliography$actors <- ReadBib("R/mod-about-actors.bib")
-  message("* Actors bibliography successfully loaded !")
-  bibliography$informatics <- ReadBib("R/mod-about-informatics.bib")
-  message("* Informatics bibliography successfully loaded !")
-  bibliography$ecology <- ReadBib("R/mod-about-ecology.bib")
-  message("* Ecology bibliography successfully loaded !")
+  # bibliography <- list()
+  # bibliography$actors <- ReadBib("R/mod-about-actors.bib")
+  # message("* Actors bibliography successfully loaded !")
+  # bibliography$informatics <- ReadBib("R/mod-about-informatics.bib")
+  # message("* Informatics bibliography successfully loaded !")
+  # bibliography$ecology <- ReadBib("R/mod-about-ecology.bib")
+  # message("* Ecology bibliography successfully loaded !")
   
   # use function
   output$actors <- renderBibliography(bibliography$actors)
