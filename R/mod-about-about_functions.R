bf0c4d9a095f98197f0137287d7373b914ef79bd
Elie Ker Arno <earnaud@outlook.com>
2020-05-05 15:32:25 +0200

    Fixed 'about' tab

--- R/mod-about-about_functions.R
+++ R/mod-about-about_functions.R
@@ -6,12 +6,28 @@
 #' @param bib file path to bibliography
 #'
 #' @export
-#' @importFrom RefManageR NoCite PrintBibliography
+#' @importFrom RefManageR ReadBib NoCite PrintBibliography
 #' @importFrom shiny renderUI withProgress incProgress HTML
 #' @importFrom utils capture.output
 renderBibliography <- function(bib) {
+  bib <- ReadBib(bib)
   NoCite(bib, "*")
+  
   renderUI(
+    # sapply(
+    #   bib,
+    #   function(b) {
+    #     incProgress(1 / length(bib))
+    #     PrintBibliography(b,
+    #       .opts = list(style = "html")
+    #     )
+    #   }
+    # ) %>% 
+    #   invisible %>% 
+    #   capture.output %>% 
+    #   paste(collapse = "") %>% 
+    #   HTML %>% 
+    #   withProgress(message = "Loading bibtex ...", value = 0)
     withProgress(message = "Loading bibtex ...", value = 0, {
       HTML(
         paste(
@@ -33,11 +49,4 @@ renderBibliography <- function(bib) {
       )
     })
   )
-}
-
-# Dev: use it to get lbiraries and version
-libsToTerminal <- function() {
-  ins <- installed.packages()[loaded_packages()$package, 1:3]
-  ins <- ins[ins[, "LibPath"] != ins["base", "LibPath"], c(1, 3)]
-  cat(paste0(ins[, "Package"], " v", ins[, "Version"], "\n"))
 }
