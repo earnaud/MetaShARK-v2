10761de91236a8963d98e4090c9b4cea8d47446d
Elie Ker Arno <earnaud@outlook.com>
2020-10-28 10:16:30 +0100

    Fix for 1.4.1: - remove doc for %grep%

--- R/utils-%grep%.R
+++ R/utils-%grep%.R
@@ -1,12 +1,4 @@
-#' %grep%
-#' 
-#' This function is a copycat from `%in%` to use grepl instead of exact match. 
-#' If `x` is found in a part of any element of `table`, returns TRUE.
-#'
-#' @param x vector or NULL: the values to be matched as a pattern.
-#' @param table vector or NULL: the values to be matched against. Long vectors
-#'  are not supported.
-#'  @param ... additional argument valid for `grepl`.
+#' @noRd
 `%grep%` <- function(x, table, ...) {
   sapply(x, function(y){
     isTRUE(any(grepl(y, table, ...)))
