6b6fb64c39a25b4673858e725aee2577e9ff08ce
Elie Ker Arno <earnaud@outlook.com>
2022-07-06 16:19:35 +0200

    Added input, output and session to allow their inspection

--- R/utils_eal_browse.R
+++ R/utils_eal_browse.R
@@ -1,6 +1,6 @@
 #' @noRd
 # shortcut for oberver in EAL pages
-.browse_dev <- function(main_env, page) {
+.browse_dev <- function(main_env, page, input, output, session) {
   observeEvent(main_env$dev_browse(), {
     if (main_env$current_tab() == "fill" && main_env$EAL$page == page)
       browser()
